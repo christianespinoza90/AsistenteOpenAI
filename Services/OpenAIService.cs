8035645a8a7b13ff5c0724da4996fb86c4cf00c2
christian.espinoza@tecnologicoliceocristiano.edu.ec <christian.espinoza@tecnologicoliceocristiano.edu.ec>
2026-08-13 23:19:34 -0500

    ok todo

--- Services/OpenAIService.cs
+++ Services/OpenAIService.cs
@@ -1,4 +1,4 @@
-﻿using AsistenteOpenAI.Interfaces;
+﻿﻿using AsistenteOpenAI.Interfaces;
 using AsistenteOpenAI.Models;
 using System;
 using System.Collections.Generic;
@@ -12,7 +12,7 @@ namespace AsistenteOpenAI.Services
     public class OpenAIService : IAsistenteIA
     {
 
-#pragma warning disable OPENAI001
+        #pragma warning disable OPENAI001
         private readonly ResponsesClient cliente;
         private readonly string modelo;
 
@@ -23,7 +23,7 @@ namespace AsistenteOpenAI.Services
                 throw new ArgumentException("El nombre del modelo no puede estar vacío.", nameof(modelo));
             }
 
-            this.cliente = new ResponsesClient( Mi clave);
+            this.cliente = new ResponsesClient("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");
             this.modelo = modelo;
         }
 
@@ -38,7 +38,7 @@ namespace AsistenteOpenAI.Services
                 $"El estudiante se llama {pregunta.Estudiante} Responde de manera clara y concisa." +
                 $"Pregunta del estudiante: {pregunta.Texto}";
 
-            ResponseResult resultado = await cliente.CreateResponseAsync(modelo, instrucciones);
+            ResponseResult resultado =await cliente.CreateResponseAsync(modelo, instrucciones);
 
             string textoRespuesta = resultado.GetOutputText();
             RespuestaIA respuestaIA = new RespuestaIA(textoRespuesta, modelo);
