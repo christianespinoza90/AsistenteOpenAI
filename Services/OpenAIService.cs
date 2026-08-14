6dcc98ba7eb7bfd8d4cef672eb1e06240cccbd29
christian.espinoza@tecnologicoliceocristiano.edu.ec <christian.espinoza@tecnologicoliceocristiano.edu.ec>
2026-08-13 20:12:40 -0500

    ok

---
+++ Services/OpenAIService.cs
@@ -0,0 +1,18 @@
+﻿using AsistenteOpenAI.Interfaces;
+using AsistenteOpenAI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsistenteOpenAI.Services
+{
+    public class OpenAIService : IAsistenteIA
+    {
+        public Task<RespuestaIA> PreguntarAsync(PreguntaIA pregunta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
