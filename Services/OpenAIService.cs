28f747212ff517b90d7608a63cb25ab3fd533161
christian.espinoza@tecnologicoliceocristiano.edu.ec <christian.espinoza@tecnologicoliceocristiano.edu.ec>
2026-08-18 20:07:07 -0500

    OK

--- Services/OpenAIService.cs
+++ Services/OpenAIService.cs
@@ -23,7 +23,7 @@ namespace AsistenteOpenAI.Services
                 throw new ArgumentException("El nombre del modelo no puede estar vacío.", nameof(modelo));
             }
 
-       
+            this.cliente = new ResponsesClient("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");
             this.modelo = modelo;
         }
 
